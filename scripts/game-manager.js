c994c3621ac73fe968863b07b131677d24e8fe9e
declan <declanstambaugh@gmail.com>
2024-10-11 09:43:57 -0600

    invisible errors.

--- scripts/game-manager.js
+++ scripts/game-manager.js
@@ -1,7 +1,8 @@
 //@ts-check
 
-import { canvas } from "./common/canvas";
-import { Player } from "./player.js";
+import { canvas } from "./common/canvas.js";
+import { Player, Player2 } from "./player.js";
+import { SimpleGoodItem } from "./collectables/good.js"
 
 export class GameManager {
     constructor() {
@@ -12,46 +13,76 @@ export class GameManager {
         this.isGameOver = false;
 
         this.goodSpawn = {
-            lastTIme: 0,
-            nextTIme: Math.min(rand(15), 5),
-            getNewNExt: function() {
-                this.nextTIme = Math.min(rand(15), 5)
-            }
+            lastTime: 0,
+            nextTime: 0,
+            next: function () {
+                this.lastTime= 0
+                this.nextTime = rand(2 * 1000, 5 * 1000)
+            },
         };
+
+        this.spawner(0); 
     }
 
     initialize() {
         this.players = [];
         let p1 = new Player(canvas.height / 2, canvas.width / 2)
-    p1.x -= p1.width / 2;
-    p1.x -= p1.height / 2;
-        this.players.push(p1);
+        p1.x -= p1.width / 2;
+        p1.y -= p1.height / 2;
+        p1.x -= 50;
+        
+        this.players = [];
+        let p2 = new Player2(canvas.height / 2, canvas.width / 2)
+        p2.x -= p2.width / 2;
+        p2.y -= p2.height / 2;
+        p2.x += 50;
+        
+        this.players.push(p2);
     }
 
     update(elapsedTime) {
+        this.spawner(elapsedTime);
+
         this.players.forEach(p => {
             p.update();
         })
+
         this.collectables.forEach(c => {
             c.update();
         })
     }
 
     spawner(elapsedTime) {
+        this.goodSpawn.lastTime += elapsedTime;
         
+        if(this.goodSpawn.lastTime > this.goodSpawn.nextTime) {
+            //spawn a good item
+            const buffer = 50
+            const sx = rand(buffer, canvas.width - buffer)
+            const sy = rand(buffer, canvas.height - buffer)
+            const item = new SimpleGoodItem(sx, sy)
+            this.collectables.push(item);
+            // reset the spawn timer and get a new spawn time
+			this.goodSpawn.next();
+			//debugger;
+        }
     }
 
     draw() {
         this.players.forEach(p => {
             p.draw();
         })
+
         this.collectables.forEach(c => {
             c.draw();
         })
     }
 }
 
-function rand(max = 100) {
-    let r = Math.floor(Math.random() * max + 1);
+function rand(min, max) {
+    let upper = max - min;
+    let r = Math.floor(Math.random() * upper) + min;
     return r;
-}r
+}
+
+console.log("does this work")
--- scripts/player.js
+++ scripts/player.js
@@ -123,3 +123,125 @@ export class Player {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+export class Player2 {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+        this.width = 75;
+        this.height = 25;
+
+        this.speed = 10;
+        
+        this.keyBindings = {
+            up: "KeyW",
+            down: "KeyS",
+            left: "KeyA",
+            right: "KeyD",
+        }
+        
+        this.wireUpKeyboard();
+    }
+
+    moving ={
+        up: false,
+        down: false,
+        left: false,
+        right: false,
+    }
+
+    wireUpKeyboard() {
+        window.addEventListener("keydown", (e) => {
+            //console.log(e);
+
+            switch(e.code) {
+                case this.keyBindings.up:
+                    this.moving.up = true;
+                    break;
+                case this.keyBindings.down:
+                    this.moving.down = true;
+                    break;
+                case this.keyBindings.left:
+                    this.moving.left = true;
+                    break;
+                case this.keyBindings.right:
+                    this.moving.right = true;
+                    break;
+            }
+        });
+
+        window.addEventListener("keyup", (e) => {
+            //console.log(e);
+            switch(e.code) {
+                case this.keyBindings.up:
+                    this.moving.up = false;
+                    break;
+                case this.keyBindings.down:
+                    this.moving.down = false;
+                    break;
+                case this.keyBindings.left:
+                    this.moving.left = false;
+                    break;
+                case this.keyBindings.right:
+                    this.moving.right = false;
+                    break;
+            }
+        });
+    }
+
+    update() {
+        let directionX = 0;
+        let directionY = 0;
+
+        if (this.moving.up) {
+            directionY = -1;
+        }
+
+        if (this.moving.down) {
+            directionY = 1;
+        }
+
+        if (this.moving.up && this.moving.down) {
+            directionY = 0
+        }
+
+        if (this.moving.left) {
+            directionX = -1;
+        }
+
+        if (this.moving.right) {
+            directionX = 1;
+        }
+
+        if (this.moving.left && this.moving.right) {
+            directionX = 0;
+        }
+
+        this.y += this.speed * directionY
+        this.x += this.speed * directionX
+
+        //prevent going off left
+        if (this.x < 0) {
+            this.x = 0
+        }
+
+        //prevent going off top
+        if (this.y < 0) {
+            this.y = 0
+        }
+
+        //prevent going off right
+        if (this.x + this.width > canvas.width) {
+            this.x = canvas.width - this.width;
+        }
+
+        //prevent going off bottom
+        if (this.y + this.height > canvas.height) {
+            this.y = canvas.height - this.height;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
